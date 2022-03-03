9b879e3372da90749928933fa8bf1056703c0744
code-diggers-369 <codediggers007@gmail.com>
2022-03-03 12:46:58 +0530

    8 - Create User With Email & Password In Firebase

--- App.js
+++ App.js
@@ -11,112 +11,25 @@ import {
   Alert,
 } from 'react-native';
 import database from '@react-native-firebase/database';
+import auth from '@react-native-firebase/auth';
 
 export default function App() {
-  const [inputTextValue, setInputTextValue] = useState(null);
-  const [list, setList] = useState(null);
-  const [isUpdateData, setIsUpdateData] = useState(false);
-  const [selectedCardIndex, setSelectedCardIndex] = useState(null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    getDatabase();
-  }, []);
-
-  const getDatabase = async () => {
+  const handleSignup = async () => {
     try {
-      // const data = await database().ref('todo').once('value');
-      const data = await database()
-        .ref('todo')
-        .on('value', tempData => {
-          console.log(data);
-          setList(tempData.val());
-        });
+      const isUserCreated = await auth().createUserWithEmailAndPassword(
+        email,
+        password,
+      );
+
+      console.log(isUserCreated);
     } catch (err) {
       console.log(err);
-    }
-  };
 
-  const handleAddData = async () => {
-    try {
-      if (inputTextValue.length > 0) {
-        const index = list.length;
-        const response = await database().ref(`todo/${index}`).set({
-          value: inputTextValue,
-        });
-
-        console.log(response);
-
-        setInputTextValue('');
-      } else {
-        alert('Please Enter Value & Then Try Again');
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleUpdateData = async () => {
-    try {
-      if (inputTextValue.length > 0) {
-        const response = await database()
-          .ref(`todo/${selectedCardIndex}`)
-          .update({
-            value: inputTextValue,
-          });
-
-        console.log(response);
-        setInputTextValue('');
-        setIsUpdateData(false);
-      } else {
-        alert('Please Enter Value & Then Try Again');
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleCardPress = (cardIndex, cardValue) => {
-    try {
-      setIsUpdateData(true);
-      setSelectedCardIndex(cardIndex);
-      setInputTextValue(cardValue);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleCardLongPress = (cardIndex, cardValue) => {
-    try {
-      Alert.alert('Alert', `Are You Sure To Delete ${cardValue} ?`, [
-        {
-          text: 'Cancel',
-          onPress: () => {
-            console.log('Cancel Is Press');
-          },
-        },
-        {
-          text: 'Ok',
-          onPress: async () => {
-            try {
-              const response = await database()
-                .ref(`todo/${cardIndex}`)
-                .remove();
-
-              setInputTextValue('');
-              setIsUpdateData(false);
-              console.log(response);
-            } catch (err) {
-              console.log(err);
-            }
-          },
-        },
-      ]);
-
-      // setIsUpdateData(true);
-      // setSelectedCardIndex(cardIndex);
-      // setInputTextValue(cardValue);
-    } catch (err) {
-      console.log(err);
+      setMessage(err.message);
     }
   };
 
@@ -125,52 +38,29 @@ export default function App() {
       <StatusBar hidden={true} />
       <View>
         <Text style={{textAlign: 'center', fontSize: 20, fontWeight: 'bold'}}>
-          Todo App
+          Metahub
         </Text>
         <TextInput
           style={styles.inputBox}
-          placeholder="Enter Any Value"
-          value={inputTextValue}
-          onChangeText={value => setInputTextValue(value)}
+          placeholder="Enter Your Email"
+          value={email}
+          onChangeText={value => setEmail(value)}
         />
-        {!isUpdateData ? (
-          <TouchableOpacity
-            style={styles.addButton}
-            onPress={() => handleAddData()}>
-            <Text style={{color: '#fff'}}>Add</Text>
-          </TouchableOpacity>
-        ) : (
-          <TouchableOpacity
-            style={styles.addButton}
-            onPress={() => handleUpdateData()}>
-            <Text style={{color: '#fff'}}>Update</Text>
-          </TouchableOpacity>
-        )}
-      </View>
-
-      <View style={styles.cardContainer}>
-        <Text style={{marginVertical: 20, fontSize: 20, fontWeight: 'bold'}}>
-          Todo List
-        </Text>
-
-        <FlatList
-          data={list}
-          renderItem={item => {
-            const cardIndex = item.index;
-            if (item.item !== null) {
-              return (
-                <TouchableOpacity
-                  style={styles.card}
-                  onPress={() => handleCardPress(cardIndex, item.item.value)}
-                  onLongPress={() =>
-                    handleCardLongPress(cardIndex, item.item.value)
-                  }>
-                  <Text>{item.item.value}</Text>
-                </TouchableOpacity>
-              );
-            }
-          }}
+        <TextInput
+          style={styles.inputBox}
+          placeholder="Enter Your Password"
+          value={password}
+          onChangeText={value => setPassword(value)}
+          secureTextEntry={true}
         />
+
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => handleSignup()}>
+          <Text style={{color: '#fff'}}>Signup</Text>
+        </TouchableOpacity>
+
+        <Text>{message}</Text>
       </View>
     </View>
   );
@@ -182,6 +72,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
   },
   inputBox: {
     width: width - 30,
@@ -195,15 +86,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 10,
     borderRadius: 50,
-  },
-  cardContainer: {
-    marginVertical: 20,
-  },
-  card: {
-    backgroundColor: '#fff',
-    width: width - 40,
-    padding: 20,
-    borderRadius: 30,
-    marginVertical: 10,
   },
 });
